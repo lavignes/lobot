b81c327f1ee9d036fd7093af0a1f0da8caac622f
Scott LaVigne <pyrated@gmail.com>
2016-03-27 18:02:03 -0700

    Update RTD theme

--- setup.py
+++ setup.py
@@ -7,9 +7,16 @@ setup(name='lobot',
       author='Scott LaVigne',
       author_email='pyrated@gmail.com',
       url='https://github.com/pyrated/lobot',
+      license='MIT',
       description='An extensible IRC bot',
       download_url='https://github.com/pyrated/lobot.git',
       packages=['lobot'],
       scripts=['bin/lobot'],
-      requires=['aiohttp']
+      requires=open('requirements.txt').read().splitlines(),
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Environment :: Console',
+            'Programming Language :: Python :: 3.5',
+      ]
       )
